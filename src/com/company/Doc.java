bd94e721d639e9df9ca18b09210ae4641733d9cf
Nikita Smorschok <nsmorschok@gmail.ru>
2022-04-04 12:53:49 +0300

    String

--- src/com/company/Doc.java
+++ src/com/company/Doc.java
@@ -8,15 +8,15 @@ public class Doc {
 
     }
 
-    public static String info() {
+    public static void info() {
         System.out.println("1) блоки из трех букв заменить на *** : \n" + doc.replace("1234-dog-4567-cat-2d3s", "1234-***-4567-***-2d3s"));
         System.out.println("2) в одну строку два первых блока по 4 цифры : \n" + (doc = doc.substring(0, 4) + doc.substring(9, 13)));
-        System.out.println("3) буквы из номера документа в нижнем регистре :  \nLetters:" + doc.join("/", "dog", "cat", "d", "s"));
+        System.out.println("3) буквы из номера документа в нижнем регистре :  \nLetters:" + doc.join("/", "dog", "cat", "d", "s").toLowerCase());
         System.out.println("4) буквы из номера документа в верхнем регистре : \nLetters:" + doc.join("/", "dog", "cat", "d", "s").toUpperCase());
         System.out.println("5) Проверить содержит ли номер документа последовательность abc  : \n" + doc.contains("abc"));
         System.out.println("6) Проверить начинается ли номер документа с последовательности 555 : \n" + doc.startsWith("555"));
         System.out.println("7) Проверить заканчивается ли номер документа напоследовательность 1a2b : \n" + doc.endsWith("1a2b"));
-        return doc;
+
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,96 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
 public class Main {
-
-
     public static void main(String[] args) {
-        Doc doc = new Doc();
-        System.out.println(Doc.info());
+        Doc.info();
+
+        String text = "my name is Bob and I like eating";
+        System.out.println("Bob talks: "+text);
+
+        StringTokenizer st = new StringTokenizer(text);
+        StringTokenizer st1 = new StringTokenizer(text);
+        int maxLengthOfWord = 0;
+        int minLengthOfWord1 = 0;
+        String myText = "";
+        String myText1 = "";
+
+        while (st.hasMoreTokens()) {
+            while (st1.hasMoreTokens()) {
+                String word1 = st1.nextToken();
+                int word1Length = word1.length();
+                if (minLengthOfWord1 == 0) {
+                    myText1 = word1;
+                    minLengthOfWord1 = word1Length;
+                } else if (word1Length < minLengthOfWord1) {
+                    minLengthOfWord1 = word1Length;
+                    myText1 = word1;
+                }
+            }
+            String word = st.nextToken();
+            int wordLength = word.length();
+            if (maxLengthOfWord == 0) {
+                myText = word;
+                maxLengthOfWord = wordLength;
+            } else if (wordLength > maxLengthOfWord) {
+                maxLengthOfWord = wordLength;
+                myText = word;
+            }
+        }
+        if (maxLengthOfWord > 0) {
+            if (minLengthOfWord1 > 0)
+                System.out.println("Min  word = " + myText1);
+
+            else {
+                System.out.println("No word in string!");
+
+            }
+            System.out.println("Max  word = " + myText);
+        } else {
+            System.out.println("No word in string!");
+
+        }
+
+        String str = "fffff ab f 1234 jkjk";
+        System.out.println(getWordWithMinimumDifferentChars(str));
+    }
+
+    public static String getWordWithMinimumDifferentChars(String src) {
+        return Arrays.stream(src.replaceAll("\\p{Punct}", "").split("\\s+"))
+                .map(PairStrDistChar::new)
+                .max(Comparator.comparing(PairStrDistChar::getDiffRealCountCharVsDistChar))
+                .toString();
+        // \p{Punct}	Пунктуация: один из !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+        // \\s+         один или больше space символов, то же что и [\n\t\r\f]
+    }
+
+    private static class PairStrDistChar {
+        String word;
+        int diffRealCountCharVsDistChar;
+
+        public PairStrDistChar(String word) {
+            this.word = word;
+            this.diffRealCountCharVsDistChar = word.length() / (int) word.chars().distinct().count();
+        }
+
+        public int getDiffRealCountCharVsDistChar() {
+            return diffRealCountCharVsDistChar;
+        }
+
+        @Override
+        public String toString() {
+            return "word='" + word + '\'' +
+                    ", countDisChar=" + diffRealCountCharVsDistChar +
+                    '}';
+        }
     }
 }
+
+
+
+
+
+
